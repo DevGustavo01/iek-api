8a82cceed1745cc8ff426b7bea2bc48c0fe3cbaf
Gustavo <gu.trindadelive@gmail.com>
2024-10-25 08:54:07 -0300

    typescript sintax implementada

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,25 @@
-import express, { Request, Response } from 'express'
+import express, { Request, Response, Express } from 'express';
 
-const app = express()
-const port = 3000
+// Definindo a constante `app` com o tipo `Express`, indicando que se trata de uma instância do servidor Express.
+const app: Express = express();
 
-app.use(express.json())
+// Definindo o número da porta com o tipo `number`, para garantir que `port` terá sempre um valor numérico.
+const port: number = 3000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!')
+// Configurando o middleware para permitir o parsing de JSON nas requisições.
+// Isso transforma automaticamente o corpo das requisições JSON em objetos JavaScript, acessíveis por `req.body`.
+app.use(express.json());
+
+
+// Definindo o tipo de `req` como `Request` e `res` como `Response` para assegurar tipagem forte no Express
+app.get('/', (req: Request, res: Response): void => {
+  // Retornando um JSON com a mensagem "Hello World!".
+  res.json({ message: 'Hello World!' })
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+
+// Inicia o servidor na porta especificada com um callback de retorno
+app.listen(port, (): void => {
+  // Exibindo uma mensagem no console quando o servidor começa a escutar na porta
+  console.log(`Example app listening on port ${port}`);
 })
